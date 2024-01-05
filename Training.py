5f239ee5f5006b00fc4dc1273b0c42f8904e9185
Y00ness <seyed.unes@yahoo.com>
2024-01-06 00:06:12 +0330

    3rd exercise

--- Training.py
+++ Training.py
@@ -4,13 +4,19 @@
 # Current_Year = 2024
 # print (Person_Info , Current_Year + 100 - int(Person_Info[1]))
 
-#### Exercise 2
-Num = input('please add a num:\n')
-while True:
-    try:
-        Num = int(Num)
-        break
-    except:
-        Num=input('please enter a number: ')
-odd_even = 'even' if Num%2==0 else 'odd'
-print (f'{Num} is {odd_even}')
+# #### Exercise 2
+# Num = input('please add a num:\n')
+# while True:
+#     try:
+#         Num = int(Num)
+#         break
+#     except:
+#         Num=input('please enter a number: ')
+# result = 'even' if Num%2==0 else 'odd'
+# result = 'can be devided by 4' if Num%4==0 else result
+# print (f'{Num} is {result}')
+
+
+# #### Exercise 3
+list_a=input('write numbers that seprated with space:\n').split()
+print([int(x) for x in list_a if int(x)<10])
